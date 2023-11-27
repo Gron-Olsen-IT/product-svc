096c540aa98ce0dbe74390e0e7d49e0412552e44
Mads Lajer <eaamalr@students.eaaa.dk>
2023-11-27 11:03:47 +0100

    Product POCO setup

--- ProductAPI/Models/Product.cs
+++ ProductAPI/Models/Product.cs
@@ -1,5 +1,8 @@
 public class Product
 {
     public string Id { get; set; }
-    public decimal Price { get; set; }
+    public string SellerId { get; set; }
+    public int Valuation { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int Status { get; set; }
 }

8ade621373760759da1ac04991ba508b0baf65d2
Mads Lajer <eaamalr@students.eaaa.dk>
2023-11-29 15:15:15 +0100

    Hopefully cleaned

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -30,7 +30,7 @@ public class ProductController : ControllerBase
         }
         catch (Exception e)
         {
-            _logger.LogError(e, "Error in Get Testing moreeeeeeeee -Lajer");
+            _logger.LogError(e, "Error in Get -Lajer");
             throw;
         }
     }

23ddfd570e3bd196802dffe34930c54ed5d1a899
Boes24 <cboes2402@gmail.com>
2023-12-16 12:03:42 +0100

    ChangeTheINFRA_CONN

--- ProductAPI/Services/InfraRepo/IInfraRepo.cs
+++
@@ -1,8 +0,0 @@
-namespace ProductAPI.Services;
-
-using System.Net;
-public interface IInfraRepo {
-    public Task<HttpStatusCode> authenticateUser(string token);
-
-    public Task<HttpStatusCode> doesUserExist(string email, string token);
-}
